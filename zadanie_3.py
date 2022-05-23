65a0f71ef0ced1cb8271202a805a465a929dd551
Mateusz Cholewa <mcholewa828@gmail.com>
2022-05-23 22:07:39 +0200

    Add for loop for sum of products

--- zadanie_3.py
+++ zadanie_3.py
@@ -8,3 +8,11 @@ lista_zakupow = {'piekarnia': ['chleb', 'bułki', 'paczek'], 'warzywniak': ['mar
 for sklep, produkty in słownik_zakupow.items():
     lista = słownik_zakupow[sklep]
     print('Wchodze do', sklep.capitalize(), 'i kupuję tam natępujące rzeczy:', lista.title())
+
+    suma = 0
+for value in lista_zakupow:
+    value_list = lista_zakupow[value]
+    policz = len(value_list)
+    suma = suma + policz
+
+print(f"W sumie kupuję {suma} produktów")
