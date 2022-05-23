7e69ec292d67ba3d4465cc9332911f06866d8644
Mateusz Cholewa <mcholewa828@gmail.com>
2022-05-23 22:05:50 +0200

    Add lista_zakupow and for loop

--- zadanie_3.py
+++ zadanie_3.py
@@ -2,3 +2,9 @@ słownik_zakupow = {
     "piekarnia": "chleb, bułki, pączek",
     "warzywniak": "marchew, seler, rukola"
 }
+
+lista_zakupow = {'piekarnia': ['chleb', 'bułki', 'paczek'], 'warzywniak': ['marchew', 'seler', 'rukola']}
+
+for sklep, produkty in słownik_zakupow.items():
+    lista = słownik_zakupow[sklep]
+    print('Wchodze do', sklep.capitalize(), 'i kupuję tam natępujące rzeczy:', lista.title())
